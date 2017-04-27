adc718dead386e05d601c4d817a42c7affbb19f3
1tsova <1tsovai@gmail.com>
2017-04-28 01:21:57 +0300

    -комментарии на русском, + генерация кода

--- PILO_lab4.py
+++ PILO_lab4.py
@@ -1,15 +1,15 @@
 class TreeNode:
-    def __init__(self): # конструктор
+    def __init__(self): 
         self.data = ""
-        self.left = ""
-        self.right = ""
+        self.left = -1
+        self.right = -1
 
     def set_data(self, data, left, right): 
         self.data = data
         self.left = left
         self.right = right        
     
-    def printNode(self): # даже не пытайс€ вникать в суть этой функции, € сама тут перестала что-то понимать
+    def printNode(self): 
         
         global d
         
@@ -18,49 +18,50 @@ class TreeNode:
         
         print('\n','   '*deep+self.data)
         
-        if (type(self.left) != (str)):
-            print('   '*deep+'L:', end =' ')
-            if ((self.left.left != '') or (self.left.right != '')):
-                self.left.printNode()
+        if  (self.left != -1):
+            if (type(self.left) != (str)):
+                print('   '*deep+'L:', end =' ')
+                if ((self.left.left != '') or (self.left.right != '')):
+                    self.left.printNode()
+                else:
+                    print(self.left.data)
             else:
-                print(self.left.data)
-        else:
-            print('   '*deep+'L:', self.left, sep = ' ')
-            
-        if type(self.right) != (str): 
-            print('   '*deep+'R:',end =' ')
-            if ((self.right.left != '') or (self.right.right != '')):
-                self.right.printNode()
+                print('   '*deep+'L:', self.left, sep = ' ')
+        if (self.right != -1):
+            if type(self.right) != (str): 
+                print('   '*deep+'R:',end =' ')
+                if ((self.right.left != '') or (self.right.right != '')):
+                    self.right.printNode()
+                else:
+                    print(self.right.data)
             else:
-                print(self.right.data)
-        else:
-            print('   '*deep+'R:', self.right, sep = ' ')
+                print('   '*deep+'R:', self.right, sep = ' ')
         d-=1
     
     def parse(self):
         s = self.data
                 
-        s = brackets(s)           # убираем внешние скобки
+        s = brackets(s)           
         
         p1, p2 = len(s)-1, len(s)+3
         i=len(s)-1
         
         
-        while i>=0:               # сложение и вычитаение (бинарное), ищем с конца
+        while i>=0:               
             if s[i] == ')':     
-                i = skip(s, i-1)+1 # пропускаем внутренности скобок
-            elif s[i] == '+' or ((s[i] == '-') and (i != 0) and (s[i-1] != '(')):   # провер€ем, что минус не унарный             
+                i = skip(s, i-1)+1 
+            elif s[i] == '+' or ((s[i] == '-') and (i != 0) and (s[i-1] != '(')):   
                 p1 = p2-2                         
                 p2 = i+1
                 
                 tempL = TreeNode()                  
                 tempR = TreeNode()
-                tempL.set_data(s[:p2-1], '', '')
-                tempR.set_data(s[p2:p1+1], '', '')
+                tempL.set_data(s[:p2-1], -1,-1)
+                tempR.set_data(s[p2:p1+1], -1,-1)
                 
                 self.set_data(s[i], tempL, tempR)
                 
-                self.left.parse()   # рекурсивно вызываем эту функцию от ее новоиспеченных листов
+                self.left.parse()   
                 self.right.parse()
                 return 0
             i-=1
@@ -68,7 +69,7 @@ class TreeNode:
         p1, p2 = len(s)-1, len(s)+3
         i=len(s)-1        
         
-        while i>=0:                         # умножение и деление
+        while i>=0:                         
             if s[i] == ')':        
                 i = skip(s, i-1)+1
             elif s[i] == '*' or s[i] == '/':                      
@@ -77,8 +78,8 @@ class TreeNode:
                 
                 tempL = TreeNode()
                 tempR = TreeNode()
-                tempL.set_data(s[:p2-1], '', '')
-                tempR.set_data(s[p2:p1+1], '', '')
+                tempL.set_data(s[:p2-1], -1, -1)
+                tempR.set_data(s[p2:p1+1], -1, -1)
                 
                 self.set_data(s[i], tempL, tempR)
                 
@@ -87,20 +88,54 @@ class TreeNode:
                 return 0
             i-=1            
         
-        if s[0] == '-':                #унаный минус
+        if s[0] == '-':                
                 tempL = TreeNode()
-                tempL.set_data(s[1:], '', '')
+                tempL.set_data(brackets(s[1:]), -1, -1)
                 
-                self.set_data('-', tempL, '')
+                self.set_data('-', tempL, -1)
                 
                 self.left.parse()
                 return 0
         return 1
     
-            
+    def generateCode(self):
+        global result
+        
+        if (not isOperator(self.data)) or (self.left == -1 and self.right == -1):
+            if isId(self.data) or isConst(self.data):
+                result.append('move eax, ' + self.data)
+            return
+        
+        if (self.data == '='):
+            self.right.generateCode()
+            result.append('mov '+ self.left.data + ', eax')
+        elif (self.data == '-') and (self.right == -1): # unary operator -
+            self.left.generateCode()
+            result.append('neg eax')
+        elif (isId(self.left.data) or isConst(self.left.data)) and (isId(self.right.data) or isConst(self.right.data)): # ID or CONST on left and right both
+            result.append('mov eax, ' + self.left.data)
+            result.append(oper(self.data) + ' eax, '+ self.right.data)
+        elif ((isOperator(self.left.data)) and (isId(self.right.data) or isConst(self.right.data))): # binaty operator on left and ID or CONST on right
+            self.left.generateCode()
+            result.append(oper(self.data) + ' eax, '+ self.right.data)
+        elif (self.data in '+*') and (isOperator(self.right.data)) and (isId(self.left.data) or isConst(self.left.data)): #commutative binary operator with ID or CONST on left and binary operator on right
+            self.right.generateCode()
+            result.append(oper(self.data) + ' eax, '+ self.left.data)
+        elif (self.data in '-/') and (isOperator(self.right.data)) and (isId(self.left.data) or isConst(self.left.data)): #uncommutative binary operator with ID or CONST on left and binary operator on right  
+            self.right.generateCode()
+            result.append('mov edx, ' + self.left.data)
+            result.append('xchg eax, edx')
+            result.append(oper(self.data) + ' eax, edx')
+        elif (isOperator(self.left.data)) and (isOperator(self.right.data)):   #both brunches are binary operators             
+            self.right.generateCode()                  
+            result.append('push eax') 
+            self.left.generateCode()
+            result.append('pop edx')
+            result.append(oper(self.data) + ' eax, edx')              
+        else:
+            return 0
 
-
-def skip(s, i):         # пропуск внутренних скобок
+def skip(s, i):         
     ch = 1
     while (i >=0) and (ch!=0):
         if s[i] == ')':
@@ -110,7 +145,7 @@ def skip(s, i):         # пропуск внутренних скобок
         i-=1        
     return i
 
-def brackets(s):        # отбрасывание внешних скобок
+def brackets(s):        
     if (s[0] == '(') and (s[len(s)-1] == ')'):
         ch=1
         i = 1
@@ -127,38 +162,76 @@ def brackets(s):        # отбрасывание внешних скобок
     else:
         return s
 
-def isId(s): # провека на то, что идентификатор норм 
+def isId(s): 
     lett = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
-    numb = '0123456789'
+    numb = '0123456789_'
     if s[0] not in lett:
         return False
     for i in range(1,len(s)):
         if s[i] not in (lett+numb):
             return False
     return True
+
+def isConst(s):
+    numb = '0123456789.'
+    if s.count('.')>1:
+        return False
+    for i in range (len(s)):
+        if s[i] not in numb:
+            return False
+    return True
+
+def isOperator(s):
+    if s in '+-*/=':
+        return True
+    else:
+        return False
+
+def oper(s):
+    if s=='+':
+        return("add")
+    elif s== '-':
+        return ("sub")
+    elif s == '*':
+        return("imul")
+    elif s == '/':
+        return("idiv") 
 print('Enter a string: ')
 S = input()
-d = -1 # глубина листа (нужно дл€ вывода)
-S = ''.join(S.split()) # убираем все пробелы
-parts = S.split('=') # раздел€ем строку по знаку "="
-if len(parts)>2:    # если частей больше 2, то и "=" было не одно
+d = -1 
+result = []
+S = ''.join(S.split()) 
+parts = S.split('=') 
+if len(parts)>2:    
     print('In this string more than one "="')
-elif len(parts)<2:  # если меньше 2, то "=" не было
+elif len(parts)<2:  
     print('This string has no "="')
-elif not(isId(parts[0])):      # если справа от "=" неправильный идентификатор
+elif not(isId(parts[0])):      
     print('It is not ID on the left of "="')
 else:
-    node = TreeNode() # создаем корень
+    node = TreeNode() 
     tempL = TreeNode()
     tempR = TreeNode()
-    tempL.set_data(parts[0], '', '') 
-    tempR.set_data(parts[1], '', '')    
+    tempL.set_data(parts[0], -1, -1) 
+    tempR.set_data(parts[1], -1, -1)    
     node.set_data('=', tempL, tempR)
-    node.right.parse()      # парсим правую часть (лева€ - идентификатор)
-    node.printNode()    
-    
+    node.right.parse()      
+#    node.printNode()  
+    node.generateCode()
+    print("\nOptimal code:")
+    for i in range (len(result)):
+        print(result[i])
 
 
-
-
-
+"""tests:
+   
+1. A=-B+(C-D)*2+(E+100)*3 
+2. ј1=D1*—1+(—2+02)*5-7
+3. X=Y*Z*(B-C)*2+7
+4. Y=(F1+F22)*10+7*(F13-F333)
+5. ј100=5*(ј+D+C)*10-D
+6. TABLE=(A+B)*R-F*(S-Y-Z)
+7. “11=(-A)*B+C*D-E10*E11
+8. Z100=Z1*(Z2+Z3)-Z4*(Z5-Z6)
+9. RES=(C10-C20)*10+5*X*(Y-100)
+"""
